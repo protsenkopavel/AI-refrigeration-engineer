35484c0d3f4a3a10e3b1d76e9d6840e037d02443
Pavel Protsenko <pavelprotsenko@outlook.com>
2026-02-28 12:38:17 +0300

    infra, structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,9 +25,9 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-liquibase")
-    implementation("org.springframework.boot:spring-boot-starter-webmvc")
 
     compileOnly("org.projectlombok:lombok")
 
